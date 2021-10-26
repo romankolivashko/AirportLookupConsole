54d68d5c10c69c4f3e12873bcf5c459ad3b4b2f5
Roman Kolivashko <rkolivashko@gmail.com>
2021-10-26 12:21:13 -0700

    first commit
    Co-authored-by: Brandon Eads <brandon181989@hotmail.com>

---
+++ Airport.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+namespace AirportLookup.Models
+{
+  public class Airport
+  {
+    public int AirportId { get; set; }
+    public string Name { get; set; }
+    public string Code { get; set; }
+    public string City { get; set; }
+    public string BiggestAirline { get; set; }
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace AirportLookup
+{
+  class Program
+  {
+    static void Main()
+    {
+      var apiCallTask = ApiHelper.ApiCall("");
+      var result = apiCallTask.Result;
+      Console.WriteLine(result);
+    }
+  }
+
+  class ApiHelper
+  {
+    public static async Task<string> ApiCall(string apiKey)
+    {
+      RestClient client = new RestClient("https://6bda-98-237-209-225.ngrok.io/api/airports");
+      RestRequest request = new RestRequest($"?api-key={apiKey}", Method.GET);
+      var response = await client.ExecuteTaskAsync(request);
+      return response.Content;
+    }
+  }
+}
